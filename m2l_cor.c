e5e7f6c9f43d153a7759839a5cd210a97ee707e0
Bill Chatfield <bill_chatfield@yahoo.com>
2026-06-28 03:56:49 -0400

    Imported from https://git.savannah.nongnu.org/git/m2c.git

---
+++ m2l_cor.c
@@ -0,0 +1,72 @@
+/* Run-time functions for Modula-2 coroutine implementation.
+ * Copyright 1991, 1993, 1994, 1997 Vladimir Makarov
+ * This file is part of m2c.
+ *
+ * m2c is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * m2c is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with m2c. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ucontext.h>
+#include <stdio.h>
+#include "m2lib.h"
+
+static ucontext_t *
+prepare_context (void)
+{
+  ucontext_t *ctx;
+  int err;
+
+  ctx = malloc(sizeof(ucontext_t));
+  if (!ctx)
+    {
+      fputs("\nno memory for coroutine context", stderr);
+      m2_halt();
+    }
+
+  err = getcontext(ctx);
+  if (err != 0)
+    {
+      fputs("\ngetcontext failed", stderr);
+      m2_halt();
+    }
+
+  return ctx;
+}
+
+void
+m2_newprocess (void (*f) (), int *sp, unsigned int size, int **cor)
+{
+  ucontext_t *ctx = prepare_context();
+  ctx->uc_stack.ss_sp = sp;
+  ctx->uc_stack.ss_size = size;
+  ctx->uc_stack.ss_flags = 0;
+  ctx->uc_link = 0;
+  makecontext(ctx, f, 0);
+  *cor = (void *) ctx;
+}
+
+void
+m2_transfer (int **cor1, int **cor2)
+{
+  int err;
+
+  if (!*cor1)
+    *cor1 = (void *) prepare_context();
+
+  err = swapcontext((void *) *cor1, (void *) *cor2);
+  if (err != 0)
+    {
+      fputs("\nswapcontext failed", stderr);
+      m2_halt();
+    }
+}
---
+++ m2lib.h
@@ -0,0 +1,55 @@
+/* Copyright 2014 Peter De Wachter
+ * This file is part of m2c.
+ *
+ * m2c is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * m2c is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with m2c. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef M2LIB_H
+#define M2LIB_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define FALSE 0
+#define TRUE 1
+
+typedef unsigned int Tset;  /* Must match the compiler */
+
+/* m2l_arrpar.c */
+extern void m2_arrpar (unsigned char **adr, unsigned int high, unsigned int elong);
+/* ml2_assarr.c */
+extern void m2_assarr (char *var, char *expr, unsigned int l);
+/* m2l_assstr.c */
+extern void m2_assstr (char *var, char *str, unsigned int vlong, unsigned int strlong);
+/* m2l_cap.c */
+extern int m2_cap (unsigned int ch);
+/* m2l_cor.c */
+extern void m2_newprocess (void (*f) (), int *sp, unsigned int size, int **cor);
+extern void m2_transfer (int **cor1, int **cor2);
+/* m2l_eq.c */
+extern int m2_eq (char *str1, char *str2, unsigned int l);
+/* m2l_halt.c */
+extern void m2_halt (void);
+/* m2l_ne.c */
+extern int m2_ne (char *str1, char *str2, unsigned int l);
+/* m2l_nites.c */
+extern int m2_nites (Tset s1, Tset s2);
+/* m2l_rngovf.c */
+extern void m2_rngovf (void);
+/* m2l_setin.c */
+extern int m2_setin (Tset s1, Tset s2);
+/* m2l_testptr.c */
+extern char *m2_testptr (char *ptr);
+
+#endif  // M2LIB_H
